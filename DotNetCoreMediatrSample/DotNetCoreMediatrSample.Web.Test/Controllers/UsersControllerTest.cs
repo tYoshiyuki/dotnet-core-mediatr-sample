d40212ed513134c4695aa2b2e11e1cac5784c8df
yTsunomoto <wasiwasimaru@gmail.com>
2019-11-10 00:00:57 +0900

    update 更新。

---
+++ DotNetCoreMediatrSample/DotNetCoreMediatrSample.Web.Test/Controllers/UsersControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DotNetCoreMediatrSample.Web.ViewModel;
+using Xunit;
+
+namespace DotNetCoreMediatrSample.Web.Test.Controllers
+{
+    public class UsersControllerTest : IClassFixture<IntegrationTestWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public UsersControllerTest(IntegrationTestWebApplicationFactory<Startup> webApplicationFactory)
+        {
+            _client = webApplicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            // Arrange
+            const string url = "/api/users/1";
+
+            // Act
+            var response = await _client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<UserViewModel>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            Assert.Equal("1", result.Id);
+            Assert.Equal("Tanaka", result.FirstName);
+            Assert.Equal("Taro", result.UserName);
+        }
+    }
+}
---
+++ DotNetCoreMediatrSample/DotNetCoreMediatrSample.Web.Test/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNetCoreMediatrSample.Domain.Domain.Users;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetCoreMediatrSample.Web.Test
+{
+    public class IntegrationTestWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+            builder.ConfigureServices(services =>
+            {
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var repository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
+                repository.Save(new User(new UserId("1"), new UserName("Taro"), new FullName("Tanaka", "Tanaka")));
+                repository.Save(new User(new UserId("2"), new UserName("Jiro"), new FullName("Suzuki", "Suzuki")));
+                repository.Save(new User(new UserId("3"), new UserName("Saburo"), new FullName("Sato", "Sato")));
+            });
+        }
+    }
+}
