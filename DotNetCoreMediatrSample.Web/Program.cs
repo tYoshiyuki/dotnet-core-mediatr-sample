21f338e5135a08c2d23060c9e08c3aad354ca62a
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2022-05-17 09:32:55 +0900

    update 更新。

--- DotNetCoreMediatrSample.Web/Program.cs
+++ DotNetCoreMediatrSample.Web/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace DotNetCoreMediatrSample.Web
 {
+    // NOTE .NET6から最小ホスティングモデルが推奨されているが、ユニットテスト用の WebApplicationFactory との互換性を考慮し、一旦そのままとしています。
     public class Program
     {
         public static void Main(string[] args)
