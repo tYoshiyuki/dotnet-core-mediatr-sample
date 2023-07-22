8170273fe634c3d7204d3c5dc1d98068d103074d
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2023-07-22 13:50:42 +0900

    update コメントを追記。

--- DotNetCoreMediatrSample.Domain/Domain/Users/IUserFactory.cs.cs
+++ DotNetCoreMediatrSample.Domain/Domain/Users/IUserFactory.cs.cs
@@ -1,7 +1,16 @@
 ﻿namespace DotNetCoreMediatrSample.Domain.Domain.Users
 {
+    /// <summary>
+    /// <see cref="User"/> のファクトリインターフェースです。
+    /// </summary>
     public interface IUserFactory
     {
+        /// <summary>
+        /// <see cref="User"/> を生成します。
+        /// </summary>
+        /// <param name="userName"><see cref="UserName"/></param>
+        /// <param name="fullName"><see cref="FullName"/></param>
+        /// <returns><see cref="User"/></returns>
         User CreateUser(UserName userName, FullName fullName);
     }
 }
