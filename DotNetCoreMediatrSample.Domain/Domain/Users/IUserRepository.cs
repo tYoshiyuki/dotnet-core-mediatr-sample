7569ccac34d338c48bb877cc2ce0cbf4b7197047
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2023-07-22 13:46:20 +0900

    update コメントを追記。

--- DotNetCoreMediatrSample.Domain/Domain/Users/IUserRepository.cs
+++ DotNetCoreMediatrSample.Domain/Domain/Users/IUserRepository.cs
@@ -2,13 +2,41 @@
 
 namespace DotNetCoreMediatrSample.Domain.Domain.Users
 {
-
+    /// <summary>
+    /// <see cref="User"/> のリポジトリインターフェースです。
+    /// </summary>
     public interface IUserRepository
     {
+        /// <summary>
+        /// <see cref="User"/> を取得します。
+        /// </summary>
+        /// <param name="id"><see cref="UserId"/></param>
+        /// <returns><see cref="User"/></returns>
         User Find(UserId id);
+
+        /// <summary>
+        /// <see cref="User"/> を取得します。
+        /// </summary>
+        /// <param name="userName"><see cref="UserName"/></param>
+        /// <returns><see cref="User"/></returns>
         User Find(UserName userName);
+
+        /// <summary>
+        /// <see cref="User"/> を全件取得します。
+        /// </summary>
+        /// <returns><see cref="User"/>のリスト</returns>
         IEnumerable<User> FindAll();
+
+        /// <summary>
+        /// <see cref="User"/> を保存します。
+        /// </summary>
+        /// <param name="user"><see cref="User"/></param>
         void Save(User user);
+
+        /// <summary>
+        /// <see cref="User"/> を削除します。
+        /// </summary>
+        /// <param name="user"><see cref="User"/></param>
         void Remove(User user);
     }
 }
